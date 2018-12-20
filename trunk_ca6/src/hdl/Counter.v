711a1bc0ff641271416e3fdfac447be3e32815ef
SaraSaffari <sarasaffari.1997@gmail.com>
2018-12-20 13:47:49 +0330

    part 2 modules are addded

--- trunk_ca6/src/hdl/Counter.v
+++ trunk_ca6/src/hdl/Counter.v
@@ -1,15 +1,15 @@
 module counterr(clk, reset, en, counter);
-reg [17:0] counter_up;
-output[17:0] counter;
+reg [2:0] counter_up;
+output[7:0] counter;
 input reset, en, clk;
 // up counter
 always @(posedge clk)
 	begin
 		if(reset)
-			counter_up <= 18'd0;
+			counter_up <= 7'd0;
 		else 
 			if (en)
-		 		counter_up <= counter_up + 18'd1;
+		 		counter_up <= counter_up + 7'd1;
 	end 
 assign counter = counter_up;
 endmodule
