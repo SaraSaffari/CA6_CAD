4d9fedefc0338b3760333b396e28ea5555d162bb
kamran <kamran2727@gmail.com>
2018-12-26 16:14:21 +0330

    we are going to cmplete dtcinverse top

--- trunk_ca6/src/hdl/dct_inverse_top.v
+++ trunk_ca6/src/hdl/dct_inverse_top.v
@@ -1,34 +1,49 @@
 module dct_inverse_top#(
 	)
 	(
-		input wire clock;
-		input wire start;
-		output wire done;
+		input wire clk,
+		input wire reset,
+		input wire start, // 
+		output wire done,
+		// *********** SRAM Interface Signals ****************************
+		// Read-only port
+		output wire [AW-1:0]	raddr,
+		input wire	[DW-1:0]	rdata,
+		// Write-only port
+		output wire [AW-1:0]	waddr,
+		output wire [DW-1:0]	wdata,
+		output wire 			wr_enable
 	);
 
 	wire mem64_done;
 	wire icdt_done;
 	wire mem32_done;
 
+
  	MEM64_16#(
 		.AW (18),
 		.DW (16)
 	)mem64(
-		.clock(clock),
+		.clock(clk),
 		.start(start),
 		.r_data(),
+		.i_read(),
+		.j_read(),
+		.mem_out(),
+		.r_addr(),
 		.done(mem64_done)
 	);
 
 
 	iCDT i(
-		.clk(clock),
+		.clk(clk),
 		.start(mem64_done),
 		.out_mem64(),
 		.out_i(),
 		.out_j(),
-		.result(),
-		.done(icdt_done)
+		.final_result(),
+		.done(icdt_done),
+		.Wen_32()
 	);
 
 
@@ -36,7 +51,7 @@ module dct_inverse_top#(
 		.AW (18),
 		.DW (16)
 	)mem32(
-		.clock(clock),
+		.clock(clk),
 		.start(icdt_done),
 		.r_data()
 		.done(done)
