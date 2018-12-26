e53ae983f4121c0e4bec69a775a6890ede56a997
SaraSaffari <sarasaffari.1997@gmail.com>
2018-12-26 16:29:31 +0330

    top module in progress

--- trunk_ca6/src/hdl/dct_inverse_top.v
+++ trunk_ca6/src/hdl/dct_inverse_top.v
@@ -15,9 +15,13 @@ module dct_inverse_top#(
 		output wire 			wr_enable
 	);
 
+	wire [7:0] final_result;
 	wire mem64_done;
 	wire icdt_done;
 	wire mem32_done;
+	wire [2:0] i , j;
+	wire [175:0] mem_out;
+	wire Wen_32; 
 
 
  	MEM64_16#(
@@ -26,11 +30,11 @@ module dct_inverse_top#(
 	)mem64(
 		.clock(clk),
 		.start(start),
-		.r_data(),
-		.i_read(),
+		.r_data(rdata),
+		.i_read(i),
 		.j_read(),
-		.mem_out(),
-		.r_addr(),
+		.mem_out(mem_out),
+		.r_addr(raddr),
 		.done(mem64_done)
 	);
 
@@ -38,12 +42,12 @@ module dct_inverse_top#(
 	iCDT i(
 		.clk(clk),
 		.start(mem64_done),
-		.out_mem64(),
-		.out_i(),
-		.out_j(),
-		.final_result(),
+		.out_mem64(mem_out),
+		.out_i(i),
+		.out_j(j),
+		.final_result(final_result),
 		.done(icdt_done),
-		.Wen_32()
+		.Wen_32(Wen_32)
 	);
 
 
@@ -53,6 +57,11 @@ module dct_inverse_top#(
 	)mem32(
 		.clock(clk),
 		.start(icdt_done),
-		.r_data()
+		.mem32_enb (Wen_32),
+		.r_data(final_result),
+		.w_data(wdata),
+		.in_icounter(i),
+		.in_jcounter(j),
+		.final_addr(waddr),
 		.done(done)
 	);
